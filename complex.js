e7eb24a321f5445c12b93b0b5103af2589216923
praczu99 <72567448+praczu99@users.noreply.github.com>
2020-12-16 22:30:24 +0100

    Add files via upload

---
+++ complex.js
@@ -0,0 +1,29 @@
+var Complex = /** @class */ (function () {
+    function Complex(re, im) {
+        this.re = re;
+        this.im = im;
+    }
+    Complex.prototype.add = function (com) {
+        var a = this.re + com.re;
+        var b = this.im + com.im;
+        return new Complex(a, b);
+    };
+    Complex.prototype.subtract = function (com) {
+        var a = this.re - com.re;
+        var b = this.im - com.im;
+        return new Complex(a, b);
+    };
+    Complex.prototype.getMod = function () {
+        return Math.sqrt((Math.pow(this.re, 2)) + (Math.pow(this.im, 2)));
+    };
+    Complex.prototype.toString = function () {
+        console.log("re: ", this.re, ", ", "im: ", this.im);
+    };
+    return Complex;
+}());
+var a = new Complex(3, 4);
+var b = new Complex(7, 4);
+var c = a.add(b);
+var d = new Complex(3, -2);
+var e = c.subtract(d);
+e.toString();
